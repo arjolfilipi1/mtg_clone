679ef2261b51e2320446e46d94e8690c4c69b66a
Filipi Arjol <filipi@forschner.com>
2026-03-30 15:41:16 +0200

    move 51%

--- scripts/classes/Card.gd
+++ scripts/classes/Card.gd
@@ -2,7 +2,7 @@ extends Control
 class_name Card
 #z_index when highlighted
 var card_index = 10
-signal pressed(Node)
+signal pressed(card: Card)
 #data of the card
 #store data from card database
 
@@ -14,26 +14,16 @@ var board_pos:Area2D = null
 var dragging = false
 #stores offset during movement
 var offset: Vector2 = Vector2.ZERO 
-	
-var hover_scale = Vector2(1.2, 1.2)  # scale when hovered
-var normal_scale = Vector2(1.0, 1.0)
-var duration:float = 0.2  # seconds for the highlight tween
-
 #if any of the cildren is highlighted
 var parts_highlighted:= false
 
 
 #nodes to handle visual and movement(also clicking
 @onready var visual : Node = $vizual
-@onready var movement: Node =  $movement
+#@onready var movement: Node =  $movement
+@onready var normal_scale: Vector2 = Vector2.ONE
+@onready var hover_scale: Vector2 = Vector2(1.2, 1.2)
 
-func _exit_tree():
-	"""Clean up when card is destroyed"""
-	# Clean up tweens from movement
-	if movement and movement.has_method("_cleanup_tweens"):
-		movement._cleanup_tweens()
-	
-	
 
 #initial setup of the card, called by the game_manager script
 func setup(data,_controller):
@@ -42,15 +32,16 @@ func setup(data,_controller):
 	visual.card = self
 	state.controller = _controller
 	state.card_node = self
-	state.player_controled = true if state.controller.is_human else false
+	state.player_controled =  state.controller.is_human 
 	state.setup(data)
-	
-	
-	
+
 #destroy card in game
 func send_to_grave():
 	# Clear highlight before removal
 	TurnManager.waiting_for_input = false
+	MovementManager.move_card_to_grave(self, global_position, _on_grave_movement_complete)
+
+func _on_grave_movement_complete(card: Card):
 	if get_parent():
 		get_parent().remove_child(self)
 	if board_pos:
@@ -58,7 +49,6 @@ func send_to_grave():
 	if UI_Manager.highlighted == self:
 		UI_Manager.highlighted = null
 	queue_free()
-
 
 	
 
@@ -81,13 +71,36 @@ func _ready():
 #send data to movement for drag etc and signals if the card is selected
 func _on_gui_input(event: InputEvent) -> void:
 	if event is InputEventMouseButton:
-		if event.button_index == MOUSE_BUTTON_LEFT:
-			if event.pressed:
-				# Don't start dragging if clicking on a button
-				
-				pressed.emit()
+		_handle_mouse_button(event)
+	elif event is InputEventMouseMotion and dragging:
+		MovementManager.animate_card_drag(self, get_global_mouse_position(), offset)
+func _handle_mouse_button(event: InputEventMouseButton):
+	if event.button_index != MOUSE_BUTTON_LEFT:
+		return
 	
-	movement.on_click(event)
+	if event.pressed:
+		pressed.emit(self)
+		_on_press()
+	else:
+		_on_release()
+func _on_press():
+	if state.player_controled:
+		if TurnManager.is_selecting_mana and not state.controller.mana_selected:
+			MovementManager.move_card_to_mana(self, state.player_controled, 
+				UI_Manager.player_mana_card_nr + 1)
+		elif TurnManager.current_phase == GameEnums.TurnEnum.MAIN and not TurnManager.waiting_for_input:
+			if state.card_location == GameEnums.CardZone.HAND:
+				_start_drag()
+
+func _on_release():
+	if dragging:
+		MovementManager.end_card_drag(self, get_global_mouse_position())
+		dragging = false
+func _start_drag():
+	dragging = true
+	offset = get_global_mouse_position() - global_position
+	TurnManager.dragging = self
+	visual.set_drag_visuals(true)
 
 func _on_mouse_entered():
 	Game_Manager.select_card(self)
